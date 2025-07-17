a63d8ba16946c96cd8b864eeed70c5d4dc0fa206
Felipe Alexandre <felipealexandre@INV001575.local>
2025-07-17 09:14:00 -0300

    feat: auth context

--- App.tsx
+++ App.tsx
@@ -5,12 +5,14 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 import theme from "./src/theme";
 
 import { SignIn } from "./src/screens/SignIn";
 
 import { Loading } from "./src/components/Loading";
+import { UserProvider } from "@realm/react";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -21,12 +23,13 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle={"light-content"}
-        backgroundColor={"transparent"}
-        translucent
-      />
-      <SignIn />
+      <AuthContextProvider fallback={SignIn}>
+        <StatusBar
+          barStyle={"light-content"}
+          backgroundColor={"transparent"}
+          translucent
+        />
+      </AuthContextProvider>
     </ThemeProvider>
   );
 }
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,49 @@
+import { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "@react-native-firebase/auth";
+
+export type AuthContextDataProps = {};
+
+type AuthContextProviderProps = {
+  fallback:
+    | React.ComponentType<unknown>
+    | React.ReactElement
+    | null
+    | undefined;
+  children: React.ReactNode;
+};
+
+export const AuthContext = createContext<AuthContextDataProps>(
+  {} as AuthContextDataProps
+);
+
+export function AuthContextProvider({
+  fallback: Fallback,
+  children,
+}: AuthContextProviderProps) {
+  const [initializing, setInitiliazing] = useState(true);
+  const [user, setUser] = useState();
+
+  function handleAuthStateChanged(user) {
+    console.log(user);
+    setUser(user);
+    setInitiliazing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = onAuthStateChanged(getAuth(), handleAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (initializing) {
+    return null;
+  }
+
+  if (!user) {
+    if (typeof Fallback === "function") {
+      return <Fallback />;
+    }
+    return <>{Fallback}</>;
+  }
+
+  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+}
